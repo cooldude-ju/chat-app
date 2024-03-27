ccffdf6ecac490ff1b848698b91865fbc56fbfa6
cooldude-ju <148276342+cooldude-ju@users.noreply.github.com>
2024-03-27 15:56:25 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-5slp.onrender.com
-", {
+			const socket = io("https://chat-app-5slp.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
